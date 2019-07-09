61e5d5af3a887ead948c5105129dd44325a174cf
Arnon D. Lieber <arnon.lieber@gmail.com>
2019-07-08 21:34:41 -0400

    bef_changing_HW

---
+++ PDA_Fall2019/Lecture_Files/PDA_Lecture_02/naming_conventions_example.R
@@ -0,0 +1,77 @@
+#-------- Naming conventions example
+#=========================================
+
+
+#-------- An example of the Camel style
+
+helloWorld
+
+helloWorlf
+
+helloWorld
+
+
+#-------- Example of Snake style
+
+hello_world_again
+
+hello_worlf_again
+
+hello_world_again
+
+#-------- An example of alternative style
+
+hello.world.again
+
+hello_worlf_again
+
+hello.world.again
+
+
+
+x <- 3 + 15
+x
+r <- "R rocks"
+r
+
+
+
+#-------- Vector recycling example
+#=========================================
+
+
+# Example 1
+u <- c(1, 3.1, 2.6, 0)
+v1 <- 2
+
+u + v1
+
+# Example 2
+u  <- c(1, 3.1, 2.6, 0)
+v2 <- c(2, 1)
+
+u + v2
+
+
+# Example 3
+u  <- c(1, 3.1, 2.6, 0, 0)
+v2 <- c(2, 1)
+
+(2*u + v2)
+
+
+# Example 4
+x <- c(12, 20.1, 53.6, 2, 7.43, 24, 8.2, 6)
+
+y <- c(x, 0, x)
+v <- 2*x + y  + 1
+
+# Summing three vectors, each with a different lenght:
+2*x ; length(2*x)
+y   ; length(y)
+1   ; length(1)
+
+# Three options to plot the result
+print(v)
+v
+(v <- 2*x + y  + 1)
